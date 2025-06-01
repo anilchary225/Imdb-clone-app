56af2a13f4347dad0e1e24e01b6bca741cf26f93
anilchary225 <vadlaanilchary225@gmail.com>
2025-06-01 23:35:08 +0530

    first commit

---
+++ imdb-app/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='h-[20vh] md:h-[60vh] bg-cover bg-center flex items-end'  style={{backgroundImage:'url(https://cdn.marvel.com/content/1x/avengersendgame_lob_mas_dsk_01.jpg)'}}>
+      <div className='text-white text-xl w-full text-center bg-blue-900/60 p-3 font-copper'>Avengers Endgame</div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ imdb-app/src/components/Movies.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+
+import axios from "axios";
+import Pagination from "./Pagination";
+
+function Movies(props) {
+  const [movies, setMovies] = useState([]);
+
+
+  const [pageNo, setPageNo] = useState(1);
+
+  const handlePrev=()=>{
+    if(pageNo == 1){
+        setPageNo(pageNo)
+    }
+    else{
+        setPageNo(pageNo-1)
+    }
+  }
+  const handleNext=()=>{
+    setPageNo(pageNo+1)
+  }
+
+  useEffect(() => {
+    
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=57290a21de804af1800e23365f2bf079&language=en-US&page=${pageNo}`
+      )
+      .then(function (res) {
+        setMovies(res.data.results);
+      });
+  }, [pageNo]);
+
+  return (
+    <div className="p-2">
+      <div className="text-2xl font-bold text-center my-4">Trending Movies</div>
+
+      <div className="m-5 my-9 flex flex-row justify-around flex-wrap gap-8">
+        {movies.map((movieObj) => {
+          return <MovieCard key={movieObj.id} movieObj={movieObj} poster_path={movieObj.poster_path} name={movieObj.title} watchList={props.watchList} handleAddtoWatchList={props.handleAddtoWatchList} handleRemoveFromWatchList={props.handleRemoveFromWatchList} />
+        })}
+      </div>
+      <div>
+        <Pagination pageNo={pageNo} handlePrev={handlePrev} handleNext={handleNext} />
+      </div>
+    </div>
+  );
+}
+
+export default Movies;
+
+// https://api.themoviedb.org/3/movie/popular?api_key=57290a21de804af1800e23365f2bf079&language=en-US&page=1
